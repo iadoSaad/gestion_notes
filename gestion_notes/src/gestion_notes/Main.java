4143c951f6b04b12624423fd1d04241ca8a6ae03
mac <mac@controller.access.network>
2022-04-22 14:59:34 +0000

    Creation etudiants

---
+++ gestion_notes/src/gestion_notes/Main.java
@@ -0,0 +1,12 @@
+package gestion_notes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Etudiant e1,e2,e3;
+		e1=new Etudiant(1, "hamid");
+		e2=new Etudiant(2, "driss");
+		e3=new Etudiant(1, "youssef");
+		
+	}
+}
