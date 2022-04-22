f6d12735ba97d36fa0ed96dd7de181d090546b15
mac <mac@controller.access.network>
2022-04-22 14:09:22 +0000

    Constructeur

--- gestion_notes/src/gestion_notes/Etudiant.java
+++ gestion_notes/src/gestion_notes/Etudiant.java
@@ -5,5 +5,11 @@ public class Etudiant {
 	private int id;
 	private String nom;
 	private double note;
-
+	
+	public Etudiant(int id,String nom){
+		this.id=id;
+		this.nom=nom;
+		this.note=10;
+	}
+	
 }
