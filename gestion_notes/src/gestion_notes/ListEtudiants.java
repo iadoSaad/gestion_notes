0ebc1cfec968408314471823b9b4b10fa9e6b992
mac <mac@controller.access.network>
2022-04-22 14:27:04 +0000

    redefinition add

--- gestion_notes/src/gestion_notes/ListEtudiants.java
+++ gestion_notes/src/gestion_notes/ListEtudiants.java
@@ -3,5 +3,11 @@ package gestion_notes;
 import java.util.ArrayList;
 
 public class ListEtudiants extends ArrayList<Etudiant> {
-
+	@Override
+	public boolean add(Etudiant e) {
+		int index =indexOf(e);
+		if(index<0) return super.add(e);
+		this.set(index, e);
+		return true;
+	}
 }
