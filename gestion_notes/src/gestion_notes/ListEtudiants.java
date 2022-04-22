94dd695f6c207cb702239e6e518932bcfc00f792
mac <mac@controller.access.network>
2022-04-22 14:54:16 +0000

    Interface Condition +filtrer

--- gestion_notes/src/gestion_notes/ListEtudiants.java
+++ gestion_notes/src/gestion_notes/ListEtudiants.java
@@ -1,6 +1,7 @@
 package gestion_notes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ListEtudiants extends ArrayList<Etudiant> {
 	@Override
@@ -9,5 +10,14 @@ public class ListEtudiants extends ArrayList<Etudiant> {
 		if(index<0) return super.add(e);
 		this.set(index, e);
 		return true;
+	}
+	
+	public List<Etudiant> filtrer(Condition c){
+		List<Etudiant> ret=new ListEtudiants();
+		for (int i = 0; i < this.size(); i++) {
+			Etudiant next = this.get(i);
+			if(c.estVrai(next)) ret.add(next);
+		}
+		return ret;
 	}
 }
