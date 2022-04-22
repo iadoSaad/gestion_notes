cd5a1d8366481eb2ca2fe12e86afe1a18b3592f9
mac <mac@controller.access.network>
2022-04-22 15:03:34 +0000

    Ajouter les Etudiants à la liste

--- gestion_notes/src/gestion_notes/Main.java
+++ gestion_notes/src/gestion_notes/Main.java
@@ -11,6 +11,10 @@ public class Main {
 		e1.setNote(15); e2.setNote(12);
 		e3.setNote(16);
 		
+		ListEtudiants mesEtudiants =new ListEtudiants();
+		mesEtudiants.add(e1); mesEtudiants.add(e2); 
+		mesEtudiants.add(e3);
+		
 		
 	}
 }
